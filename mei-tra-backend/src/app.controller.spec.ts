51c41f085e8e149eed142669ca2a7078a3373b00
Hikaru Endo <hika.blue91@gmail.com>
2025-09-14 12:06:03 +0900

    test: update tests for authentication changes

--- mei-tra-backend/src/app.controller.spec.ts
+++ mei-tra-backend/src/app.controller.spec.ts
@@ -15,8 +15,10 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "NestJS WebSocket Server is Running"', () => {
+      expect(appController.getHello()).toBe(
+        'NestJS WebSocket Server is Running',
+      );
     });
   });
 });
