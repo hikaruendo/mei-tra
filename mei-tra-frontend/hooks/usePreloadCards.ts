b840d926d81ae756390b5cdabdcba48854ab89ef
Hikaru Endo <hika.blue91@gmail.com>
2026-09-20 18:35:41 +0900

    カード画像の先読みとタンツェン表記を修正

--- mei-tra-frontend/hooks/usePreloadCards.ts
+++ mei-tra-frontend/hooks/usePreloadCards.ts
@@ -1,25 +1,17 @@
 'use client';
 
 import { useEffect } from 'react';
-import { DENSHO_ART } from '@meitra/game-client/card-art';
+import { CARD_ART_IDS, DENSHO_ART } from '@meitra/game-client/card-art';
 import { useCardDesign } from '@/contexts/CardDesignContext';
-import { CARD_BACK_PATH } from '../lib/utils/cardMapping';
-
-const ALL_SUITS = ['S', 'H', 'D', 'C'];
-const ALL_RANKS = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
 
 export function usePreloadCards() {
   const design = useCardDesign();
   useEffect(() => {
-    const paths = [
-      ...ALL_RANKS.flatMap(r => ALL_SUITS.map(s => `/cards/${r}_${s}.svg`)),
-      '/cards/joker_red.svg',
-      '/cards/joker_black.svg',
-      CARD_BACK_PATH,
-      ...(design === 'densho'
-        ? Object.keys(DENSHO_ART).map(id => `/cards/densho/${id}.jpg`)
-        : []),
-    ];
+    const paths = design === 'densho'
+      ? CARD_ART_IDS.map(id => Object.prototype.hasOwnProperty.call(DENSHO_ART, id)
+        ? `/cards/densho/${id}.jpg`
+        : `/cards/densho/styled/${id}.webp`)
+      : [...CARD_ART_IDS.map(id => `/cards/${id}.svg`), '/cards/joker_black.svg'];
     paths.forEach(src => {
       const img = new Image();
       img.src = src;
